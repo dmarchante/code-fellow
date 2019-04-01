b3c795c3cc48e6b7d1609b919712d7213e578a9e
David Marchante <cdmarchante@gmail.com>
2019-04-01 11:31:45 -0700

    update for latest lessons

---
+++ 201/class-06/class-06-lesson.js
@@ -0,0 +1,51 @@
+// Primitive Data Types
+// string, number, boolean
+
+// JavaScript Objects
+
+// key-value pairs
+// properties-values
+
+// Object literal notation
+
+var demi = {
+  name: 'Demi',
+  isAGoodDog: true,
+  numberOfLegs: 3,
+  siblings: [
+    'BuddyCat',
+    'Trillian',
+    'Aloysius',
+    'Agador Spartacus'
+  ],
+  address: {
+    street: '123 Dog Street',
+    city: 'Seattle',
+    state: 'Washington',
+    zipcode: '98116',
+  },
+  colors: [
+    'black',
+    'white'
+  ],
+  breed: 'Border Collie',
+}
+
+var preferredProp = 'siblings';
+
+for (let i = 0; i < demi.siblings.length; i++){
+  console.log(demi.siblings[i]);
+}
+
+for (let i in demi.siblings) {
+  console.log(demi.siblings[i]);
+}
+
+var agador = {};
+
+agador.name = 'Agador Spartacus';
+agador.species = 'feline';
+agador.paws = 4;
+agador.isASweetKitten = true;
+
+agador
---
+++ 201/syllabus.md
@@ -0,0 +1,21 @@
+## 201
+---
+1. Intro to Git
+2. DOM (Vanilla)
+3. Events
+  * Library
+  * Persistence
+4. Project
+
+## 301
+---
+1. DOM+
+  * jQuery
+  * Templating
+2. Serve-side
+  * Node.js
+  * SQL
+  * API's
+3. Full-stack (CRUD)
+  * Sever-side Templating (React)
+4. Project
